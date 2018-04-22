cb05f29ec22a15925aa9a5a008d65b9f4eecbd8a
Farbod Salimi <farbod_salimi@yahoo.com>
2018-04-21 23:58:49 -0700

    Remove positions

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import Notifications from './components/Notifications';
 import NotificationContainer from './components/NotificationContainer';
 import NotificationManager from './components/NotificationManager';
 import { TYPES as NotificationTypes } from './constants/types';
-import { POSITIONS as NotificationPositions } from './constants/positions';
 
 export {
   Notifications,
   NotificationContainer,
   NotificationManager,
-  NotificationTypes,
-  NotificationPositions
+  NotificationTypes
 };
+
 export default Notifications;
