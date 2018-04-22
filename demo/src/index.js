6017dccd33b8bbe12cf7ba2a5c57f622161289ad
Farbod Salimi <farbod_salimi@yahoo.com>
2018-04-22 00:27:58 -0700

    Remove bootstrap package and update demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import './demo.css';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
